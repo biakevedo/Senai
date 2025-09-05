d8cbcf1e87ce7bb16e13a2c7468993f562d27e61
FIC <fic@info.local>
2025-09-04 21:45:10 -0300

    atualização

---
+++ API/api/src/main/java/br/com/ecommerce/api/controller/ProdutoController.java
@@ -0,0 +1,32 @@
+package br.com.ecommerce.api.controller;
+
+import br.com.ecommerce.api.model.Produto;
+import br.com.ecommerce.api.service.ProdutoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/produto")
+
+
+public class ProdutoController {
+    // controller > service
+
+    private ProdutoService produtoService;
+    public ProdutoController(ProdutoService service){
+        produtoService = service;
+    }
+    // Listar todos
+    @GetMapping
+    public ResponseEntity<List<Produto>> findAll(){
+        // pegar a lista de clientes
+        List<Produto> produtos = produtoService.listarTodos();
+
+        return ResponseEntity.ok(produtos);
+    }
+
+}
---
+++ API/api/src/main/java/br/com/ecommerce/api/service/ProdutoService.java
@@ -0,0 +1,31 @@
+package br.com.ecommerce.api.service;
+
+
+import br.com.ecommerce.api.model.Produto;
+import br.com.ecommerce.api.repository.ProdutoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static org.antlr.v4.runtime.tree.xpath.XPath.findAll;
+
+@Service
+
+public class ProdutoService {
+    private final ProdutoRepository produtoRepository;
+
+    public ProdutoService(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    // listar todos os clientes
+    public List<Produto> listarTodos(){
+
+        return produtoRepository.findAll();
+    }
+
+
+
+}
+
+
